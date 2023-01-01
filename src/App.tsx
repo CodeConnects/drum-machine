60fadf10f9d0242489383cb97e08965143e71d72
oca <codeconnects@gmail.com>
2023-01-01 12:41:17 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import styles from './App.module.scss'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className={styles.container}></div>
+  )
+}
+
+export default App
