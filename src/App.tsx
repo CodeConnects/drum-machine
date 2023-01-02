30dbc0962a587407edbbb0cf131407b4f440cbb5
oca <codeconnects@gmail.com>
2023-01-02 13:06:02 -0500

    music grid structure made from checkboxes

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,22 @@ export default function App({ samples, numOfSteps }:Props) {
   return (
     <div className={styles.container}>
       <div className={styles.grid}>
-        <div className={styles.row}>
-
+        <div className={styles.cellList}>
+          {trackIds.map((trackId) => (
+            <div key={trackId} className={styles.row}>
+              {stepIds.map((stepId) => {
+                const id = trackId + "-" + stepId;
+                return (
+                  <label key={id} className={styles.cell}>
+                    <input id={id} type="checkbox" className={styles.cell__input} />
+                    <div className={styles.cell__content}></div>
+                  </label>
+                );
+              })}
+            </div>
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
