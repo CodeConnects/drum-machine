d79c8826683a1e9de675a14a14635895d132767e
oca <codeconnects@gmail.com>
2023-01-07 20:14:54 -0500

    add parameter type

--- src/components/SampleLabels.tsx
+++ src/components/SampleLabels.tsx
@@ -2,9 +2,8 @@ import styles from './SampleLabels.module.scss';
 
 export default function SampleLabels(samples: any) {
     return(
-          {/* Sound Labels */}
-          <div className={styles.soundLabels}>
-          {samples.map((sample) => (<div>{sample.name}</div>))}
+        <div className={styles.soundLabels}>
+          {samples.map((sample: any) => (<div>{sample.name}</div>))}
         </div>
     );
 }
