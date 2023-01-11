4b8730fd09441063f24eaab71118099ad6f2c6d5
oca <codeconnects@gmail.com>
2023-01-10 20:37:42 -0500

    remove component

--- src/components/SampleLabels.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles/SampleLabels.module.scss'
-
-export default function SampleLabels(samples) {
-    return(
-        <div className={styles.soundLabels}>
-          {samples.map((sample) => (<div>{sample.name}</div>))}
-        </div>
-    )
-}
