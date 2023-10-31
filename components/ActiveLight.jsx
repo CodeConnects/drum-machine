9d580930d139af47786f9d188f9422559985833f
CodeConnects <codeconnects@gmail.com>
2023-10-31 13:03:34 -0400

    modify the component to product a single light

--- components/ActiveLight.jsx
+++ components/ActiveLight.jsx
@@ -1,27 +1,24 @@
+import React from 'react';
 
 import styles from '../styles/ActiveLight.module.scss'
 
-const ActiveLight = ({ stepIds, activeRef }) => {
+const ActiveLight = ({ stepId, activeRef }) => {
 
   return(
-    <div className={styles.row}>
-      {stepIds.map(stepId => (
-        <label key={stepId} className={styles.active}>
-          <input 
-            type="radio" 
-            name="active" 
-            id={"active-" + stepId} 
-            disabled
-            ref={(elm) => {
-              if (!elm) return;
-              activeRef.current[stepId] = elm;
-            }}
-            className={styles.active__input}
-          />
-          <div className={styles.active__content} ></div>
-        </label>
-      ))}
-    </div>
+    <label key={stepId} className={styles.active}>
+      <input 
+        type="radio" 
+        name="active" 
+        id={"active-" + stepId} 
+        disabled
+        ref={(elm) => {
+          if (!elm) return;
+          activeRef.current[stepId] = elm;
+        }}
+        className={styles.active__input}
+      />
+      <div className={styles.active__content} ></div>
+    </label>
   )
 };
 
